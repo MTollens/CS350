41b71e9f2e179ea13243f3d129ba447d47e74f16
thompsom <thompsom@clarkson.edu>
2021-11-01 14:37:14 -0400

    added new classes for logical purposes

---
+++ pantry.py
@@ -0,0 +1,4 @@
+class Pantry():
+    def __init__(self):
+        pass
+
---
+++ recipe.py
@@ -0,0 +1,8 @@
+class Recipe():
+    def __init__(self, owner):
+        self.owner = owner
+        pass
+
+    def save(self):
+        pass
+
