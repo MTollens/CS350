24e77c652379eb297564b71af4b18ed69a85993b
Michael Horick <horickmj@clarkson.edu>
2021-12-02 12:59:20 -0500

    execution with update user

--- pages/execution.py
+++ pages/execution.py
@@ -12,16 +12,22 @@ class Execution(wx.Panel):
         self.Back_Button = wx.Button(parent=self, label="Back", pos=(0, 0), size=(50, 50))
         self.Back_Button.Bind(wx.EVT_BUTTON, parent.setPrevious)
 
+        self.recipe = self.parent.user.open_recipe
+
         # UI implementation here:
         self.page_name = wx.StaticText(parent=self, label="Make a Recipe", pos=(120, 20))
         self.tools_req = wx.StaticText(parent=self, label="Tools Required:", pos=(10, 100))
         self.ingredients_req = wx.StaticText(parent=self, label="Ingredients Required:", pos=(10, 180))
+        self.instructions_text = wx.StaticText(parent=self, label="Instructions:", pos=(10, 280))
         #the following fields will be filled with user data
-        self.recipe_name = wx.StaticText(parent=self, label="", pos=(20, 70))
+        self.recipe_name = wx.StaticText(parent=self, pos=(20, 70))
         self.tools_list = wx.TextCtrl(parent=self, pos=(10, 120), size=(200, 60), style=wx.TE_READONLY | wx.TE_MULTILINE)
         self.tools_list.Show()
         self.ingredients_list = wx.TextCtrl(parent=self, pos=(10, 200), size=(200, 60), style=wx.TE_READONLY | wx.TE_MULTILINE)
         self.ingredients_list.Show()
+        self.image = wx.StaticBox(parent=self, pos=(400, 20), size=(200,200))
+        self.instructions_list = wx.TextCtrl(parent=self, pos=(10, 300), size=(500, 200), style=wx.TE_READONLY | wx.TE_MULTILINE)
+        self.instructions_list.Show()
 
         # load in user dataManagement
         self.update_user()
@@ -33,6 +39,17 @@ class Execution(wx.Panel):
     # gets called when a panel is reloaded, not required to do anything but must be here
     # this is where user information should be loaded in
     def update_user(self):
-        #self.tools_list.SetValue( LOADED FROM RECIPE )
-        #self.ingredients_list.SetValue( LOADED FROM RECIPE )
-        pass
+        self.recipe = self.parent.user.open_recipe
+        self.recipe_name.SetLabel(self.recipe.title)
+
+        self.tools_temp = ""
+        for x in self.recipe.tools:
+            self.tools_temp += x + '\n'
+        self.tools_list.SetValue(self.tools_temp)
+
+        self.ingredients_temp = ""
+        for x in self.recipe.ingredients:
+            self.ingredients_temp += x + '/n'
+        self.ingredients_list.SetValue(self.ingredients_temp)
+
+        #self.image = (self.recipe.image)
