c082c87f0ce474945258cd1d59f3ef300f7e4dc1
Michael Horick <horickmj@clarkson.edu>
2021-12-03 14:39:57 -0500

    execution with steps/metric included

--- pages/execution.py
+++ pages/execution.py
@@ -1,3 +1,5 @@
+import sys
+
 import wx
 
 # not yet implemented in any way
@@ -19,16 +21,24 @@ class Execution(wx.Panel):
         self.tools_req = wx.StaticText(parent=self, label="Tools Required:", pos=(10, 100))
         self.ingredients_req = wx.StaticText(parent=self, label="Ingredients Required:", pos=(10, 180))
         self.instructions_text = wx.StaticText(parent=self, label="Instructions:", pos=(10, 280))
+
         #the following fields will be filled with user data
         self.recipe_name = wx.StaticText(parent=self, pos=(20, 70))
+
         self.tools_list = wx.TextCtrl(parent=self, pos=(10, 120), size=(200, 60), style=wx.TE_READONLY | wx.TE_MULTILINE)
         self.tools_list.Show()
+
         self.ingredients_list = wx.TextCtrl(parent=self, pos=(10, 200), size=(200, 60), style=wx.TE_READONLY | wx.TE_MULTILINE)
         self.ingredients_list.Show()
-        self.image = wx.StaticBox(parent=self, pos=(400, 20), size=(200,200))
-        self.instructions_list = wx.TextCtrl(parent=self, pos=(10, 300), size=(500, 200), style=wx.TE_READONLY | wx.TE_MULTILINE)
-        self.instructions_list.Show()
 
+        self.image = wx.StaticBox(parent=self, pos=(400, 20), size=(200,200))
+
+        self.instructions_list = wx.ListCtrl(parent=self, pos=(10, 300), size=(700, 300), style=wx.LC_REPORT)
+        self.instructions_list.InsertColumn(col=0, heading='Step')
+        self.instructions_list.InsertColumn(col=1, heading='Instructions')
+
+        self.Units = wx.Button(parent=self, label=self.get_unit_label(), pos=(400, 220), size=(100,50))
+        self.Units.Bind(wx.EVT_BUTTON, self.change_units)
         # load in user dataManagement
         self.update_user()
 
@@ -42,11 +52,31 @@ class Execution(wx.Panel):
         self.recipe = self.parent.user.open_recipe
         self.recipe_name.SetLabel(self.recipe.title)
 
-        self.tools_temp = ""
+        self.tools_parse = ""
         for x in self.recipe.tools:
-            self.tools_temp += x + '\n'
-        self.tools_list.SetValue(self.tools_temp)
+            self.tools_parse += x + '\n'
+        self.tools_list.SetValue(self.tools_parse)
 
         self.ingredients_list.SetValue(self.recipe.ingredients.pretty())
 
+        #timer functionality will have to be added
+        self.steps = enumerate(self.recipe.instructions, start=1)
+        for x in self.steps:
+            index = self.instructions_list.InsertItem(sys.maxsize, x[0])
+            self.instructions_list.SetItem(index, 0, str(x[0]))
+            self.instructions_list.SetItem(index, 1, x[1])
+        self.instructions_list.SetColumnWidth(col=1, width=wx.LIST_AUTOSIZE)
+
         #self.image = (self.recipe.image)
+
+    def change_units(self, event=None):
+        self.parent.user.metric = not(self.parent.user.metric)
+        self.parent.user.change_units()
+        self.Units.SetLabel(self.get_unit_label())
+        self.ingredients_list.SetValue(self.recipe.ingredients.pretty())
+
+    def get_unit_label(self):
+        if self.parent.user.metric:
+            return "Metric"
+        else:
+            return "Imperial"
